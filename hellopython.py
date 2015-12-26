91aabc1e26a57e8a7daf21bc71129cf7646a3574
Thomas Ridd <twridd@gmail.com>
2015-12-26 23:42:52 +0000

    added base file

---
+++ hellopython.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from json import dumps
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello python server is running'
+
+@app.route("/rest", methods=['GET'])
+def get_hello():
+
+    # Message object
+    msg = {"message":"Hello world!","greetings":["Ho ho ho!","Simple REST rocks"]}
+
+    # Return JSON response
+    return dumps(msg)
+
+if __name__ == "__main__":
+    app.run()
